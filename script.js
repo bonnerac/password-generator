9d01c4ebb68b1fce9f912db158420192b7feb5c6
bonnerac <alex.c.bonner@gmail.com>
2019-12-14 20:55:51 -0600

    added more code to the js file

--- script.js
+++ script.js
@@ -1,47 +1,80 @@
 // Variables for Character Sets
-var Special =  " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-var Numbers = "1234567890";
-var LowerCase = "abcdefghijklmnopqrstuvwxyz";
-var UpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var Special =  ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "`", "{", "|", "}", "~"];
+var Numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var LowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s","t", "u", "v", "w", "x", "y", "z"];
+var UpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+// Other Global Variables
+var isOp1;
+var isOp2;
+var isOp3;
+var isOp4;
+var isOp5;
+var possibleOptions = [];
 
 // Write a function that confirms which options the user would like to enable.
+// After all the confirms are completed, invoke optionBuilder()
 function confirms() {
   isOp1 = confirm("Would you like to include special characters?");
   isOp2 = confirm("Would you like to include numbers?");
   isOp3 = confirm("Would you like to include lower case letters?");
   isOp4 = confirm("Would you like to include upper case letters?");
+  isOp5 = prompt("Select a number of characters for your password (between 8 and 128).")
   optionBuilder();
 };
 
-confirms()
+// Option Builder's purpose is to validate that the user has enabled at least one of the options as well as to add the elements of the chosen option to the possible options array. When options have been added and validated, invoke charCheck.
+// If the user has not enabled any option, alert them to do so then send them back to the confirms function to recomplete it.
 
+function optionBuilder() {
+  if (isOp1 || isOp2 || isOp3 || isOp4) {
+      if (isOp1) {
+          possibleOptions = possibleOptions.concat(Special)
+      };
+      
+      if (isOp2) {
+          possibleOptions = possibleOptions.concat(Numbers)
+      };
+      
+      if (isOp3) {
+          possibleOptions = possibleOptions.concat(LowerCase)
+      };
 
+      if (isOp4) {
+        possibleOptions = possibleOptions.concat(UpperCase)
+    };
+      charCheck();
+  } else {
+      alert("You must select at least one character type.");
+      confirms();
+  }
+};
 
-
-
-
-
-
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-  copyBtn.removeAttribute("disabled");
-  copyBtn.focus();
+// Checks that the number of characters is between 8 and 128. If it is a number outside of the range it asks them to pick a correct number and sends them back to the confirm function. A correct number will invoke getString.
+function charCheck(){
+  if (isOp5 < 8 || isOp5 > 128){
+    alert("Please select a character length between 8 and 128.");
+    confirms();
+    }
+      else{
+        getString();
+      }
 }
 
-function copyToClipboard() {
-  // BONUS 
-}
+// Random number generator using the number of characters that the user submitted in isOp5.
+function randomNumber() {
+  return Math.floor(Math.random() * isOp5)
+};
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
-// BONUS EVENT LISTENER
+function getString() {
+  var text = "";
+  
+  for (var i = 0; i < isOp5; i++) {
+      text+=possibleOptions[randomNumber()]
+  };
+  alert(text);
+};
+
+
+confirms();
