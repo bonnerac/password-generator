173d13b75e56746e0208ee80776ccd83c6248215
bonnerac <alex.c.bonner@gmail.com>
2020-04-02 16:01:51 -0500

    removed modals

--- script.js
+++ script.js
@@ -23,49 +23,6 @@ var textBox = document.getElementById("password");
 
 
 
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function () {
-  modal.style.display = "block";
-}
-
-
-// Test confirm/deny
-var conBtn = document.getElementById("confirmBtn")
-conBtn.onclick = function () {
-  console.log("Confirm")
-}
-var denBtn = document.getElementById("denyBtn")
-denBtn.onclick = function () {
-  console.log("Deny")
-}
-
-// When user clicks yes or no, save it in a variable
-// var answer = this.getAttribute('data-name')
-// console.log(answer)
-
-
-
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
 
 
 // Initialize confirms function on button click.
